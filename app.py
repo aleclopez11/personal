699610ddbe014af440b8281fa33c0cbc3b7a24f1
Alec Lopez <aleclopez@gmail.com>
2026-04-26 16:02:01 -0400

    adding websocket for continued refresh

--- app.py
+++ app.py
@@ -1,10 +1,58 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+import asyncio
+from contextlib import asynccontextmanager
 import stocks
+import datetime
 import to_do_list
 
+async def refresh_data():
+    while True:
+        print('running refresh of stock data')
+        now = datetime.datetime.now()
+        weekday = now.weekday()
+        time = now.time()
+        start_time = datetime.time(9, 30)  # 9:30 AM
+        end_time = datetime.time(16, 0)    # 4:00 PM
+        if weekday < 5 and (start_time <= time <= end_time):
+            print('refreshing data')
+            stocks.retrieve_stocks()
+        await asyncio.sleep(300)  # Refresh every 5 minutes (300 seconds)
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Start the background task when the application starts
+    task = asyncio.create_task(refresh_data())
+    yield
+    # Cancel the background task when the application shuts down
+    task.cancel()
 
+
+app = FastAPI(lifespan = lifespan)
+
+origins = [
+    "http://localhost:5173",
+    # Add other origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # Allows specific origins
+    allow_credentials=True,         # Allows cookies to be sent cross-origin
+    allow_methods=["*"],            # Allows all methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],            # Allows all headers
+)
+
+
+@app.websocket('/ws')
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        print(f'Received data from client: {data}')
+        # Process the received data and send a response back to the client
+        response = f'Server received: {data}'
+        await websocket.send_text(response)
 
 
 @app.get('/')
@@ -23,6 +71,15 @@ def home():
     for task in tasks:
         output += f'<br>{task}'
     return(f'app homepage <br>{output}')
+    # return stock_info
+
+@app.get('/stocks')
+def get_stocks():
+    stock_info = stocks.retrieve_stocks()
+    print(type(stock_info))
+    print(stock_info)
+    print('api hit')
+    return stock_info
 
 @app.post('/addTask')
 def post_task(task: str):
